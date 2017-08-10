d21d563ea040a12f16f2f1a604e1bb3c7c66b2a1
差不多先生 <zcboy95@gmail.com>
2017-08-10 15:33:28 +0000

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>@yield('title', 'Laravel')-Laravel5.5学习</title>
+	</head>
+	<body>
+		@yield('content')
+	</body>
+</html>
+
+
+
+
+
+
